2662f034eefb1ad638797981591c166fd5b3584f
camiloparrax8 <72416477+camiloparrax8@users.noreply.github.com>
2026-09-18 12:40:12 -0500

    feat: añadir seguimiento de Google Analytics (GA4)

---
+++ components/atoms/GoogleAnalytics.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    gtag?: (...args: unknown[]) => void;
+  }
+}
+
+type GoogleAnalyticsProps = {
+  measurementId: string;
+};
+
+export function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.gtag?.("config", measurementId, {
+      page_path: pathname,
+    });
+  }, [measurementId, pathname]);
+
+  return null;
+}
